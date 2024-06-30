a048b2a3e77b11b7124b4da88ae6e509142262aa
dinesh11515 <dineshaitham2@gmail.com>
2024-06-30 11:57:01 +0530

    added api calls in token page

--- src/data/DummyData.ts
+++ src/data/DummyData.ts
@@ -1,90 +1,89 @@
-import { TAssetTableItem, TLendingBorrowingTableItem } from "@/types/dataTable";
-import { TProtocolName } from "@/types/protocol";
-  
-export const AssetTableDummyData : TAssetTableItem[] = [
-  {
-    id: "0",
-    assetSymbol: "WETH",
-    totalApys: "8%.9",
-    assetSupplyBoostedApys: "4.6%",
-    chainId: "10",
-    protocolName: TProtocolName.AAVE,
-    points: ["ezPoints", "LXP-L"]
-  },
-  {
-    id: "1",
-    assetSymbol: "WETH",
-    totalApys: "6.8%",
-    chainId: "42161",
-    assetSupplyBoostedApys: "4.6%",
-    protocolName: TProtocolName.Balancer,
-    points: ["ezPoints", "LXP-L"]
-  },
-  {
-    id: "2",
-    assetSymbol: "WETH",
-    totalApys: "9.3%",
-    chainId: "10",
-    assetSupplyBoostedApys: "1.3%",
-    protocolName: TProtocolName.COMPOUND,
-    points: ["ezPoints", "LXP-L", ]
-  },
-  {
-    id: "3",
-    assetSymbol: "WETH",
-    totalApys: "8%.9",
-    assetSupplyBoostedApys: "4.6%",
-    chainId: "10",
-    protocolName: TProtocolName.AAVE,
-    points: ["ezPoints", "LXP-L"]
-  },
-  {
-    id: "4",
-    assetSymbol: "WETH",
-    totalApys: "6.8%",
-    chainId: "42161",
-    assetSupplyBoostedApys: "1.02%",
-    protocolName: TProtocolName.Balancer,
-    points: ["ezPoints", "LXP-L"]
-  },
-  {
-    id: "5",
-    assetSymbol: "WETH",
-    totalApys: "9.3%",
-    chainId: "10",
-    assetSupplyBoostedApys: "1.3%",
-    protocolName: TProtocolName.COMPOUND,
-    points: ["ezPoints", "LXP-L", ]
-  },
- 
-] 
+import { TAssetTableItem, TLendingBorrowingTableItem } from '@/types/dataTable';
+import { TProtocolName } from '@/types/protocol';
 
-export const EthDerivativesDummyData : TAssetTableItem[] = [
+export const AssetTableDummyData: TAssetTableItem[] = [
   {
-    id: "0",
-    assetSymbol: "WETH",
-    totalApys: "8%.9",
-    assetSupplyBoostedApys: "4.6%",
-    chainId:  "10",
+    id: '0',
+    assetSymbol: 'WETH',
+    assetSupplyApy: '8.9%',
+    assetSupplyBoostedApys: '4.6%',
+    chainId: '10',
     protocolName: TProtocolName.AAVE,
-    points: ["ezPoints", "LXP-L"]
+    points: ['ezPoints', 'LXP-L'],
   },
   {
-    id: "1",
-    assetSymbol: "cbETH",
-    totalApys: "6.8%",
-    chainId: "42161",
+    id: '1',
+    assetSymbol: 'WETH',
+    assetSupplyApy: '6.8%',
+    chainId: '42161',
+    assetSupplyBoostedApys: '4.6%',
     protocolName: TProtocolName.Balancer,
-    points: ["ezPoints", "LXP-L"],
-    assetSupplyBoostedApys: "1.32%"
+    points: ['ezPoints', 'LXP-L'],
   },
   {
-    id: "2",
-    assetSymbol: "rsETH",
-    totalApys: "9.3%",
-    chainId: "10",
-    assetSupplyBoostedApys: "1.3%",
+    id: '2',
+    assetSymbol: 'WETH',
+    assetSupplyApy: '9.3%',
+    chainId: '10',
+    assetSupplyBoostedApys: '1.3%',
     protocolName: TProtocolName.COMPOUND,
-    points: ["ezPoints", "LXP-L", ]
+    points: ['ezPoints', 'LXP-L'],
   },
-] 
+  {
+    id: '3',
+    assetSymbol: 'WETH',
+    assetSupplyApy: '8%.9',
+    assetSupplyBoostedApys: '4.6%',
+    chainId: '10',
+    protocolName: TProtocolName.AAVE,
+    points: ['ezPoints', 'LXP-L'],
+  },
+  {
+    id: '4',
+    assetSymbol: 'WETH',
+    assetSupplyApy: '6.8%',
+    chainId: '42161',
+    assetSupplyBoostedApys: '1.02%',
+    protocolName: TProtocolName.Balancer,
+    points: ['ezPoints', 'LXP-L'],
+  },
+  {
+    id: '5',
+    assetSymbol: 'WETH',
+    assetSupplyApy: '9.3%',
+    chainId: '10',
+    assetSupplyBoostedApys: '1.3%',
+    protocolName: TProtocolName.COMPOUND,
+    points: ['ezPoints', 'LXP-L'],
+  },
+];
+
+export const EthDerivativesDummyData: TAssetTableItem[] = [
+  {
+    id: '0',
+    assetSymbol: 'WETH',
+    assetSupplyApy: '8%.9',
+    assetSupplyBoostedApys: '4.6%',
+    chainId: '10',
+    protocolName: TProtocolName.AAVE,
+    points: ['ezPoints', 'LXP-L'],
+  },
+  {
+    id: '1',
+    assetSymbol: 'cbETH',
+    assetSupplyApy: '6.8%',
+    chainId: '42161',
+    protocolName: TProtocolName.Balancer,
+    points: ['ezPoints', 'LXP-L'],
+    assetSupplyBoostedApys: '1.32%',
+  },
+  {
+    id: '2',
+    assetSymbol: 'rsETH',
+    assetSupplyApy: '9.3%',
+    chainId: '10',
+    assetSupplyBoostedApys: '1.3%',
+    protocolName: TProtocolName.COMPOUND,
+    points: ['ezPoints', 'LXP-L'],
+  },
+];
