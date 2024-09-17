40ac70e0c1070065d748954378c43e1323114a02
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-09-17 11:55:55 +0530

    feat: add polygon and bsc support

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 
 import { cookieStorage, createStorage } from 'wagmi';
-import { base, arbitrum, optimism } from 'wagmi/chains';
+import { base, arbitrum, optimism, linea, bsc, polygon } from 'wagmi/chains';
 
 export const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_ID;
 
@@ -10,12 +10,12 @@ if (!projectId) throw new Error('Project ID is not defined');
 const metadata = {
   name: 'Spreadefi',
   description: 'Omnichain lending and borrowing platform',
-  url: 'https://web3modal.com', 
+  url: 'https://web3modal.com',
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
 };
 
 // Create wagmiConfig
-const chains = [base, arbitrum, optimism] as const;
+const chains = [base, arbitrum, optimism, linea, bsc, polygon] as const;
 export const config = defaultWagmiConfig({
   chains,
   projectId,
